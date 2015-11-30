9f0cbbef628ca30bac4d3ad53f600ebac58bac29
Vaishaal Shankar <vaishaal@berkeley.edu>
2015-11-29 20:23:06 -0800

    added pca loss

--- sgd.py
+++ sgd.py
@@ -7,19 +7,19 @@ class BasicStochasticGradientMethod(object):
         Attributes:
             w0 (np.ndarray): Starting w_0 (defaults to 0)
             eta_update (fn[int -> float]) : (optional) The eta update rule (defaults to constant 0.01)
-            num_iter (int): (optional) Number of iterations of sgd to run (defaults to 100)
+            state_check (fn[BasicStochasticGradientMethod -> None]) : Logs current state
         '''
 
-    def __init__(self, w0, eta_update=None, num_iter=100):
+    def __init__(self, w0, eta_update=None, state_check=None):
         if (eta_update == None):
             self.eta_update = lambda x: 0.01
         else:
             self.eta_update = eta_update
         self.w = w0
-        self.num_iter = num_iter
         self._train_iter = 0
+        self.state_check = state_check
 
-    def train(self, gradients):
+    def train(self, gradients, with_replacement=False):
         '''
             Train with list of gradient
 
@@ -27,12 +27,14 @@ class BasicStochasticGradientMethod(object):
                 self (BasicStochasticGradientMethod): Object
                 gradients (List[fn[np.ndarray -> float]]): List of gradient functions at various examples
         '''
-        w = self.w
-        for i,g in enumerate(np.random.permutation(gradients)):
-           prev_w = w
-           w = w - self.eta_update(self._train_iter)*g(w)
-           self._train_iter += 1
-        self.w = w
+        if with_replacement:
+            shuffled_gradients = np.random.choice(gradients, len(gradients))
+        else:
+            shuffled_gradients = np.random.permutation(gradients)
+        for g in shuffled_gradients:
+            self.train_one(g)
+            if (self.state_check):
+                self.state_check(self)
 
     def train_one(self, gradient):
         '''
@@ -45,5 +47,18 @@ class BasicStochasticGradientMethod(object):
 
         self.w = self.w - self.eta_update(self._train_iter)*gradient(self.w)
         self._train_iter += 1
+
+class ShamirStochasticGradientMethod(BasicStochasticGradientMethod):
+    '''
+        Stochastic gradient descent class for Shamir's SGD algorithm for PCA
+
+        Attributes:
+            w0 (np.ndarray): Starting w_0 (defaults to 0)
+            eta_update (fn[int -> float]) : (optional) The eta update rule (defaults to constant 0.01)
+        '''
+
+    def train_one(self, gradient):
+        super(ShamirStochasticGradientMethod, self).train_one(gradient)
+        self.w = self.w/np.linalg.norm(self.w)
 
 
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
-from sgd import BasicStochasticGradientMethod
+from sgd import BasicStochasticGradientMethod, ShamirStochasticGradientMethod
 from loss import EmpiricalLossFn
+from sklearn.decomposition import PCA
 import numpy as np
 import sklearn.datasets
 
@@ -57,3 +58,31 @@ def hinge_loss_test():
 
     assert objective(sgd.w) == 0
     assert sum(np.sign(data.dot(sgd.w)) == labels) == 360
+
+def pca_loss_test():
+    #This is nonconvex!!!
+
+    digits = sklearn.datasets.load_digits(2)
+    X = digits.data
+    w_star = np.linalg.svd(X)[-1][0]
+
+    def pca_grad(w, x):
+        x = x[np.newaxis].T
+        return -x.dot(x.T).dot(w)
+
+    def pca_objective(w, x):
+        x = x[np.newaxis].T
+        return -w.dot(x.dot(x.T)).dot(w)
+
+    pca_loss = EmpiricalLossFn(pca_objective, pca_grad)
+    objective = pca_loss.partial_eval_objective_full(X)
+    gradients = [pca_loss.partial_eval_gradient_streaming(x) for x in X]
+    shuffled_gradients = np.random.permutation(gradients)
+
+    w_0 = np.random.rand(*X[0].shape)
+    w_0 = w_0/np.linalg.norm(w_0)
+
+    sgd = ShamirStochasticGradientMethod(w_0, lambda x: 1e-3/(x+1))
+    sgd.train(np.random.permutation(gradients), False)
+    sgd.train(np.random.permutation(gradients), False)
+    assert np.linalg.norm(sgd.w - w_star) < 0.01
